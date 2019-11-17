319620f2970d3c398a78019763c8387507047cfb
Trần Vũ Hiệp <vuhiep.tran95>
2019-11-17 23:49:06 +0700

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ MVCClient1/Controllers/CallBackController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCClient1.Controllers
+{
+    public class CallBackController: Controller
+    {
+        public ActionResult Index(string code)
+        {
+            return Content(code);
+        }
+    }
+}
